4d48adfeb3051d29f8d67b9971b883f387894857
Saif Hassan <saifhassan451@gmail.com>
2024-09-23 16:22:00 +0500

    07reactRouter

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,60 @@
 import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
-createRoot(document.getElementById('root')).render(
+// const router = createBrowserRouter([
+//   {
+//      path: '/',
+//      element: <Layout/>,
+//      children: [
+//       {
+//         path: "",
+//         element: <Home/>
+//       },
+//       {
+//         path: "about",
+//         element: <About/>
+//       }, 
+//       {
+//         path: "contact",
+//         element: <Contact/>
+//       }, 
+//      ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+     <Route path='' element={<Home/>}/>
+     <Route path='about' element={<About/>}/>
+     {/* <Route path='about/saif' element={<Github/>} /> // here need to add new page with path or element
+     </Route> */}
+     <Route path='contact' element={<Contact/>}/>
+     <Route path='user/:userid' element={<User/>}/>
+     <Route
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github/>}/>
+
+    </Route>
+  )
+)
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
