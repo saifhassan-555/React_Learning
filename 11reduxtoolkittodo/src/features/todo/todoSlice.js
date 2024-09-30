76c63909967e29f5470654f83d7ae783bc8d9d4c
Saif Hassan <saifhassan451@gmail.com>
2024-09-30 15:15:11 +0500

    11reduxtoolkittodo

---
+++ 11reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -0,0 +1,27 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{id: 1, text: "Hello world"}]
+}
+
+export const todoSLice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {  // it has two things one is property and other is functions. Two important key words "state or action"
+         addTodo: (state, action) => {
+            const todo ={
+                id: nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
+         },
+         removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+         },
+
+    }   
+})
+
+export const {addTodo, removeTodo} = todoSLice.actions
+
+export default todoSLice.reducer
